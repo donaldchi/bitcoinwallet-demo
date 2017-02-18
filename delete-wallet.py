d9e6aff9a1905abdbc9c50772fa294c58f2a96a1
donaldchi <chiguanglong0@gmail.com>
2017-02-18 18:41:40 +0900

    Delete delete-wallet.py

--- delete-wallet.py
+++
@@ -1,8 +0,0 @@
-from blockcypher import delete_wallet
-
-is_deleted = delete_wallet(wallet_name='chi', api_key='0b955a57f12548209b090dfc5a4c4a72', is_hd_wallet=True)
-print(is_deleted)
-
-is_deleted = delete_wallet(wallet_name='donaldchi', api_key='0b955a57f12548209b090dfc5a4c4a72', is_hd_wallet=True)
-print(is_deleted)
-
